7d2483ec04370b583f10c000cb0d1e1f4c7547a4
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-10 14:56:05 -0600

    Add init script

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,17 @@
 
 const {exec} = require('child_process');
 const package = require('../package.json');
+const FS = require('fs')
+const Path = require('path')
 
 const command = process.argv[2];
 
-const allowedCommands = ['lint', 'lint-fix', 'preinstall', 'precommit'];
+const allowedCommands = ['lint', 'lint-fix', 'preinstall', 'precommit', 'init'];
 
 try {
-  if (allowedCommands.includes(command)) {
+  if (command === 'init') {
+    init()
+  } else if (allowedCommands.includes(command)) {
     exec(package.scripts[command], {
       stdio: {
         stdin: process.stdin,
@@ -27,4 +31,28 @@ try {
 } catch (error) {
   console.error(error);
   process.exit(1);
+}
+
+async function init() {
+  const files = [
+    '.eslintignore',
+    '.eslintrc.json',
+    '.gitignore',
+    '.prettierignore',
+    '.prettierrc',
+    'tsconfig.json'
+  ]
+
+  console.log(await Promise.all(files.map(async file => {
+    const src = Path.join(__dirname, '..', file)
+    const dst = Path.join(process.cwd(), file)
+
+    try {
+      await FS.promises.copyFile(src, dst, FS.constants.COPYFILE_EXCL)
+    } catch (error) {
+      if (error.code !== 'EEXIST') { throw error }
+    }
+
+    return dst
+  })))
 }
