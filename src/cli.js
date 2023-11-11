6e23ca0a24ee59de004b9bc2befbf2f1a9e39bd2
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-11 08:34:39 -0600

    v0.3.0

--- src/cli.js
+++ src/cli.js
@@ -4,33 +4,37 @@
 
 const {exec} = require('child_process');
 const package = require('../package.json');
-const FS = require('fs')
-const Path = require('path')
+const FS = require('fs');
+const Path = require('path');
 
-const command = process.argv[2];
+if (command === 'init') {
+  init();
+} else {
+  script(process.argv[2]);
+}
 
-const allowedCommands = ['lint', 'lint-fix', 'preinstall', 'precommit', 'init'];
+async function script(command) {
+  const allowedCommands = ['lint', 'lint-fix', 'preinstall', 'precommit', 'init'];
 
-try {
-  if (command === 'init') {
-    init()
-  } else if (allowedCommands.includes(command)) {
-    exec(package.scripts[command], {
-      stdio: {
-        stdin: process.stdin,
-        stdout: process.stdout,
-        stderr: process.stderr,
-      },
-    });
+  if (allowedCommands.includes(command)) {
+    try {
+      exec(package.scripts[command], {
+        stdio: {
+          stdin: process.stdin,
+          stdout: process.stdout,
+          stderr: process.stderr,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
   } else {
     console.error(
       `${command} is not a valid command. Valid commands are: ${allowedCommands.join(',')}`,
     );
     process.exit(1);
   }
-} catch (error) {
-  console.error(error);
-  process.exit(1);
 }
 
 async function init() {
@@ -40,19 +44,29 @@ async function init() {
     '.gitignore',
     '.prettierignore',
     '.prettierrc',
-    'tsconfig.json'
-  ]
+    'tsconfig.json',
+  ];
 
-  console.log(await Promise.all(files.map(async file => {
-    const src = Path.join(__dirname, '..', file)
-    const dst = Path.join(process.cwd(), file)
+  console.log(
+    await Promise.all(
+      files.map(async file => {
+        const src = Path.join(__dirname, '..', file);
+        const dst = Path.join(process.cwd(), file);
 
-    try {
-      await FS.promises.copyFile(src, dst, FS.constants.COPYFILE_EXCL)
-    } catch (error) {
-      if (error.code == 'EEXIST') { console.warn('Not overwriting ' + file) } else { throw error }
-    }
+        try {
+          await FS.promises.copyFile(src, dst, FS.constants.COPYFILE_EXCL);
+        } catch (error) {
+          if (error.code == 'EEXIST') {
+            console.warn(`Not overwriting ${file}`);
+          } else {
+            throw error;
+          }
+        }
 
-    return dst
-  })))
+        return dst;
+      }),
+    ),
+  );
+
+  await script('install-precommit');
 }
