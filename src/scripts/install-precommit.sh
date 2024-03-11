ac2f40c929d92d97088bb9f9f707a0e5d72f3fee
Lucas Wojciechowski <hello@lucaswoj.com>
2024-03-11 09:04:44 -0700

    Update typescript dependencies

--- src/scripts/install-precommit.sh
+++ src/scripts/install-precommit.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -f .git/hooks/pre-commit ]; then
-  echo 'Not overwriting .git/hooks/pre-commit';
+  echo -e 'Not overwriting .git/hooks/pre-commit';
 else
-  echo '#!/bin/sh\nyarn run precommit' > .git/hooks/pre-commit;
+  echo -e '#!/bin/sh\nyarn run precommit' > .git/hooks/pre-commit;
   chmod +x .git/hooks/pre-commit;
   echo 'Added .git/hooks/pre-commit';
 fi
