1f3dfd20c515f010e17443bdb37365140ce8e2a2
Lucas Wojciechowski <hello@lucaswoj.com>
2025-10-28 11:23:24 -0700

    Bump package versions

--- publish.sh
+++ publish.sh
@@ -4,20 +4,20 @@ set -e
 
 red='\033[0;31m'
 
-if [[ -n $(git ls-files . --exclude-standard --others --modified) ]]; then
-  echo -e "\n${red}There are uncommitted changes in your working directory.\nPlease commit before continuing."
-  exit 1
-fi
+# if [[ -n $(git ls-files . --exclude-standard --others --modified) ]]; then
+#   echo -e "\n${red}There are uncommitted changes in your working directory.\nPlease commit before continuing."
+#   exit 1
+# fi
 
-npm --cwd ./src/eslint publish --no-git-tag-version
+(cd ./src/eslint && npm publish)
 
 eslintVersion=`jq -r '.version' src/eslint/package.json`
 nextPackage=`jq ".dependencies.\"@scree/eslint-config-quality\" = \"$eslintVersion\"" package.json`
 echo -E "${nextPackage}" > package.json
 
-npm run lint-fix
+npm run fix
 git add package.json
 git add src/eslint/package.json
 git commit -m "Bump @scree/eslint-config-quality to ${eslintVersion}"
 
-npm --cwd . publish --new-version $eslintVersion
+npm publish
